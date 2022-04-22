fb80bdb1f2786423a3dc52369da0d0b1309673ad
Jimmy <jimmy.tang@langlive.com>
2022-04-22 10:39:04 +0800

    fix

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,21 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from 'next'
-import fetch from 'node-fetch';
-
 
 const hello = (req: NextApiRequest, res: NextApiResponse) => {
-  fetch('https://tw.rter.info/capi.php')
-  .then(function(response) {
-    console.log(response)
-    return response.json();
-  })
-  .then(function(myJson) {
-    res.status(200).json(myJson)
-    console.log(myJson);
-  })
-  .catch(e => {
-    console.warn(e)
-  })
+  res.status(200).json({ name: 'John Doe' })
 }
 
 export default hello
